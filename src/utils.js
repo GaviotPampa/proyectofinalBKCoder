70a3e09ab5a2be83490356ff863b4ba7dc0a3ef5
Gabriela Carolina Lopez Carrizo <gaviclcarrizo@gmail.com>
2023-11-28 22:05:53 -0300

    nuevos camnios en el archivo utils sobre connection string y config

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import { dirname } from 'path';
 import { fileURLToPath } from "url";
 export const __dirname = dirname(fileURLToPath(import.meta.url));
-import config from './config/config.js';
+
 import MongoStore from 'connect-mongo';
 import './persistence/daos/mongodb/db/dbConnection.js';
 
